9816439829152481a8df72901aee80847cb8972b
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-10-23 18:50:06 +0330

    wire internal state machine pt2

--- tests/classification/test_accuracy.py
+++ tests/classification/test_accuracy.py
@@ -6,3 +6,4 @@ class TestAccuracy(unittest.TestCase):
     def test_perfect_accuracy(self):
         y_true = np.array(['A', 'B', 'C'])
         y_pred = np.array(['A', 'B', 'C'])
+        self.assertAlmostEqual(Accuracy().calculate(y_true, y_pred), 1.0)
