d5ee5aa12f2dc64584a3db1caef8363c0dc81439
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-10-25 10:28:17 +0330

    implement core engine pt2

--- tests/classification/test_cohens_kappa.py
+++ tests/classification/test_cohens_kappa.py
@@ -11,3 +11,4 @@ class TestCohensKappa(unittest.TestCase):
         y_pred = np.array([1, 2, 3, 1, 2])
         self.assertAlmostEqual(self.metric.calculate(y_true, y_pred), 1.0)
 
+    def test_no_agreement_above_chance(self):
